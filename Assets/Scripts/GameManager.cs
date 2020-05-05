d83f34bb77e1671283d6d4f789cd02a03ad4f612
sandnine <sandnine@gmail.com>
2020-05-05 16:05:38 -0300

    Alteracoes
    Fonte, Ponto Estrela

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
     public int pontuacao;
 
+    public Text pontuacaoTexto;
+
     // Start is called before the first frame update
     void Start()
     {
         pontuacao = 0;
+        this.mostrarPontos();
     }
 
     // Update is called once per frame
@@ -22,5 +26,10 @@ public class GameManager : MonoBehaviour
     public void AumentarPontuacao()
     {
         pontuacao += 1;
+        this.mostrarPontos();
+    }
+
+    private void mostrarPontos(){
+        this.pontuacaoTexto.text = pontuacao.ToString();
     }
 }
